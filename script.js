09f73b4fcb503df959b5b3a5eb807899a1833b43
Madhan <madhankhons@gmail.com>
2026-03-11 22:59:58 +0530

    Initial project upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function validateForm(){
+
+let name = document.getElementById("name").value;
+
+if(name==""){
+alert("Please enter your name");
+return false;
+}
+
+alert("Registration Successful!");
+return true;
+
+}
